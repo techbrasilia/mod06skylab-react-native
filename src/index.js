5619b2d5c63e2ecb266be91e1ca21786fc4ab0ff
Fabio <techbrasilia@outlook.com>
2020-04-13 19:16:36 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+
+// console.tron.log('teste');
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </NavigationContainer>
+  );
+}
